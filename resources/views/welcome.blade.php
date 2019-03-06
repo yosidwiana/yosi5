47a89a4d9796463ab7eee4164f4e8ebacfd93d4a
yosidwiana <yosidwiana1@gmail.com>
2019-03-06 11:02:37 +0700

    firstcommit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>YosiDwiAna</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="assets/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/css/util.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+			
+
+	<div class="flex-w flex-str size1 overlay1">
+		<div class="flex-w flex-col-sb wsize1 bg0 p-l-65 p-t-37 p-b-50 p-r-80 respon1">
+			<div class="wrappic1">
+				<a href="#">
+					<img src="assets/images/logo.jpg" alt="IMG">
+				</a>
+			</div>		
+			<div class="w-full flex-c-m p-t-80 p-b-90">
+				<div class="wsize2">
+					<h3 class="l1-txt1 p-b-34 respon3">
+						Welcome!!
+					</h3>
+
+					<p class="m2-txt1 p-b-46">
+						Follow us for update now!
+					</p>
+
+					<form class="contact100-form validate-form m-t-10 m-b-10">
+						<div class="wrap-input100 validate-input m-lr-auto-lg" data-validate = "Email is required: ex@abc.xyz">
+							<input class="s2-txt1 placeholder0 input100 trans-04" type="text" name="email" placeholder="Email Address">
+
+							<button class="flex-c-m ab-t-r size2 hov1">
+								<i class="zmdi zmdi-long-arrow-right fs-30 cl1 trans-04"></i>
+							</button>
+						</div>		
+					</form>
+				</div>
+			</div>
+			
+			<div class="flex-w">
+				<a href="https://id-id.facebook.com/login/" class="size3 flex-c-m how-social trans-04 m-r-15 m-b-10">
+					<i class="fa fa-facebook"></i>
+				</a>
+
+				<a href="https://twitter.com/login?lang=id" class="size3 flex-c-m how-social trans-04 m-r-15 m-b-10">
+					<i class="fa fa-twitter"></i>
+				</a>
+
+				<a href="https://www.youtube.com/watch?v=PPGYwREzPGI" class="size3 flex-c-m how-social trans-04 m-r-15 m-b-10">
+					<i class="fa fa-youtube-play"></i>
+				</a>
+			</div>
+		</div>
+			
+
+		<div class="wsize1 simpleslide100-parent respon2">
+			<!--  -->
+			<div class="simpleslide100">
+				<div class="simpleslide100-item bg-img1" style="background-image: url('assets/images/horor.jpg');"></div>
+				<div class="simpleslide100-item bg-img1" style="background-image: url('assets/images/komedi.jpg');"></div>
+				<div class="simpleslide100-item bg-img1" style="background-image: url('assets/images/roman.jpg');"></div>
+                <div class="simpleslide100-item bg-img1" style="background-image: url('assets/images/sejarah.jpg');"></div>
+                <div class="simpleslide100-item bg-img1" style="background-image: url('assets/images/kartun.png');"></div>
+			</div>
+		</div>
+	</div>
+
+
+
+	
+
+<!--===============================================================================================-->	
+	<script src="assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor/bootstrap/js/popper.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor/tilt/tilt.jquery.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>
