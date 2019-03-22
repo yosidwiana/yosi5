47614a1acba23c0afd3e0cd0e404fea7fdf7ad6b
yosidwiana <yosidwiana1@gmail.com>
2019-03-22 11:58:33 +0700

    master

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,25 +1,12 @@
 @extends('layouts.master')
 @section('content')
-
-    <section class="content-header">
-      <h1>
-        Simple Tables
-        <small>preview of simple tables</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Tables</a></li>
-        <li class="active">Simple</li>
-      </ol>
-    </section>
-
     <!-- Main content -->
     <section class="content">
       <div class="row">
         <div class="col-md-6">
           <div class="box">
             <div class="box-header with-border">
-              <h3 class="box-title">Bordered Table</h3>
+              <h3 class="box-title">KATEGORI FILM TERBARU</h3>
               <form action="#" method="get" class="sidebar-form">
                   <div class="input-group">
                     <input type="text" name="q" class="form-control" placeholder="Search...">
